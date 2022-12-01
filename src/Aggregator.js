871265eb942ae307d5d4c964656cb1aaebd6de41
peepingpenguin198 <115051003+peepingpenguin198@users.noreply.github.com>
2022-11-30 21:33:05 -0500

    Add files via upload

---
+++ src/Aggregator.js
@@ -0,0 +1,78 @@
+export default function Aggregator(props) {
+    const { countMenuItems, menuItems, onAdd, onRemove } = props;
+    const totalCost = menuItems.reduce((a,c) => a + c.qty * c.cost, 0 );
+    const totalPrepTime = menuItems.reduce((a,c) => a + c.qty * c.preptime, 0 );
+    const totalCookTime = menuItems.reduce((a,c) => a + c.qty * c.cooktime, 0 );
+    return (
+        <div className = "block col-1">
+            <div className="block-header">
+                <h3>Menu Summary</h3>
+            </div>
+            <aside className="block col-1">
+                <div className="row">
+                    <div className="col-1"> <h4>Dishes</h4> </div>
+                    <div className="col-1"> <h4>Adjust quantity</h4> </div>
+                    <div className="col-1 text-right"> <h4>Units & Costs</h4> </div>
+                </div>
+                <div>
+                    {menuItems.map((item) => (
+                        <div key={item.id} className="row">
+                            <div className="col-1">{item.name}</div>
+                            <div className="col-1">
+                                <button className="button button-dark remove" onClick={() => onRemove(item)}>
+                                        -
+                                    </button>
+                                <span className="number-padding">{item.qty}</span>
+                                <button className="button button-dark add" onClick={() => onAdd(item)}>
+                                    +
+                                </button>
+                            </div>
+                            <div className="col-1 text-right">
+                                {item.qty} x ${item.cost.toFixed(2)}
+                            </div>
+                        </div>
+                    ))}
+                <>
+                <hr/>
+                    <div className="row">
+                        <div className="col-2">
+                            <b>Total Cost: </b> 
+                            ${totalCost.toFixed(2)}
+                                <br/>
+                            <b>Total Prep Time:</b> {totalPrepTime.toFixed(2)} hrs
+                                <br/>
+                            <b>Total Cook Time:</b> {totalCookTime.toFixed(2)} hrs
+                        </div>
+                        {/* <div className="col-1 text-right">
+                            ${totalCost.toFixed(2)}
+                            <br/>
+                            {totalPrepTime.toFixed(2)} hrs
+                            <br/>
+                            {totalCookTime.toFixed(2)} hrs
+                        </div> */}
+
+                    </div>
+                </>
+                </div>
+                {/* if countMenuItems isn't  0, show total # */}
+                {/* <a>
+                {countMenuItems? (
+                <button className="badge">{countMenuItems}</button>
+            ) : (
+                ''
+            )}
+                </a> */}
+            {/* <p>
+                <button className="button button-dark">
+                    Clear Items
+                </button>
+            </p> */}
+            </aside>
+            {/* <div className="row">
+                {dishes.map((dises) => (
+                    <div>{dishes.name}</div>
+                ))}
+            </div> */}
+        </div>
+    );
+}
---
+++ src/Dish.js
@@ -0,0 +1,38 @@
+export default function Dish(props) {
+    const { item, dish, onAdd, onRemove } = props;
+    return (
+        <div className="card block-card" alt="card">
+            <img className="image-card" src={dish.image} alt={dish.image} />
+            <h3>
+                {dish.name}
+            </h3>
+            <div className="dish-info" alt="dish-info">
+                <b>Prep Time:</b> {dish.preptime} hrs
+            </div>
+            <div className="dish-info" alt="dish-info">
+                <b>Cook Time:</b> {dish.cooktime} hrs
+            </div>
+            <div className="dish-info" alt="dish-info">
+                <b>Unit Cost:</b> ${dish.cost.toFixed(2)} 
+            </div>
+            <div className="button-block">  
+                <p> 
+                    {/* Servings: */}
+                    {item ? (
+                        <div> 
+                            <button className="button button-dark remove" onClick={() => onRemove(item)}>
+                                -
+                            </button>
+                            <span className="number-padding">{item.qty}</span>
+                            <button className="button button-dark add" onClick={() => onAdd(item)}>
+                                +
+                            </button>
+                        </div>
+                    ) : (
+                        <button className="button button-dark" onClick={() => onAdd(dish)}>Add to Menu</button>
+                    )}
+                </p> 
+            </div>
+        </div>
+    )
+}
